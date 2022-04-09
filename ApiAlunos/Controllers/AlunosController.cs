657e779a0f33bdded5ad6c5b696b34396221de03
Davidson Bezerra <davidsonbss@live.com>
2022-04-09 08:39:26 -0300

    Inicio do projeto ApiAlunos

---
+++ ApiAlunos/Controllers/AlunosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAlunos.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunosController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public String Get()
+        {
+            return "Teste de saida";
+        }
+    }
+}
